c77d712879ab6f6ce78b14d0fdc3e73af18e385e
javadjahangiri <javad.jahangiri.niopdc@gmail.com>
2022-02-16 20:32:04 +0330

    create super user

--- crud_ajax/views.py
+++ crud_ajax/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import ListView
+from .models import CrudUser
+
+
+class CrudView(ListView):
+    model = CrudUser
+    template_name = 'crud_ajax/crud.html'
+    context_object_name = 'users'
