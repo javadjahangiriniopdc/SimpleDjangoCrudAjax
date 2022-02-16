c2391068d9ef4cda7e24c24b03f9db87b6399855
javadjahangiri <javad.jahangiri.niopdc@gmail.com>
2022-02-16 20:51:49 +0330

    add create user

--- SimpleDjangoCrudWithAjax/urls.py
+++ SimpleDjangoCrudWithAjax/urls.py
@@ -18,4 +18,6 @@ from crud_ajax import views
 
 urlpatterns = [
     path('crud/', views.CrudView.as_view(), name='crud_ajax'),
+    path('ajax/crud/create/', views.CreateCrudUser.as_view(), name='crud_ajax_create'),
+
 ]
--- crud_ajax/views.py
+++ crud_ajax/views.py
@@ -10,3 +10,28 @@ class CrudView(ListView):
     model = CrudUser
     template_name = 'crud_ajax/crud.html'
     context_object_name = 'users'
+
+
+from .models import CrudUser
+from django.views.generic import View
+from django.http import JsonResponse
+
+
+class CreateCrudUser(View):
+    def get(self, request):
+        name1 = request.GET.get('name', None)
+        address1 = request.GET.get('address', None)
+        age1 = request.GET.get('age', None)
+
+        obj = CrudUser.objects.create(
+            name=name1,
+            address=address1,
+            age=age1
+        )
+
+        user = {'id': obj.id, 'name': obj.name, 'address': obj.address, 'age': obj.age}
+
+        data = {
+            'user': user
+        }
+        return JsonResponse(data)
