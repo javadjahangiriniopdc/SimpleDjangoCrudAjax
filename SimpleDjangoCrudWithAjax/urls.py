17e457a4097c6bae270b92ed05a21219dcbe7868
javadjahangiri <javad.jahangiri.niopdc@gmail.com>
2022-02-16 20:58:19 +0330

    add update user

--- SimpleDjangoCrudWithAjax/urls.py
+++ SimpleDjangoCrudWithAjax/urls.py
@@ -19,5 +19,5 @@ from crud_ajax import views
 urlpatterns = [
     path('crud/', views.CrudView.as_view(), name='crud_ajax'),
     path('ajax/crud/create/', views.CreateCrudUser.as_view(), name='crud_ajax_create'),
-
+    path('ajax/crud/update/', views.UpdateCrudUser.as_view(), name='crud_ajax_update'),
 ]
